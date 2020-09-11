cb505675430e4038c6954557a1afb80305a2a6b3
Robert Fletcher <lobatifricha@gmail.com>
2020-09-10 20:39:52 -0700

    refactor node classes out of Composer

--- lib/buttress/composer.rb
+++ lib/buttress/composer.rb
@@ -1,6 +1,80 @@
 require 'pathname'
 
 module Buttress
+  class BaseNode
+    attr_accessor :raw_node, :parent_node
+
+    def initialize(raw_node, parent_node: nil)
+      raise "missing node" unless raw_node
+      self.raw_node = raw_node
+      self.parent_node = parent_node
+    end
+  end
+
+  class RootNode < BaseNode
+    def find_class(class_name)
+      ClassNode.new(find_class_node(raw_node, class_name))
+    end
+
+    def find_class_node(node, class_name)
+      return unless node
+      return node if node.type == :class &&
+        node.children.first.children.last.to_s == class_name
+
+      current_node = node
+      class_name.split('::').each do |name_part|
+        current_node = current_node.children.detect do |child|
+          next unless child
+          child.type == :class &&
+          child.children.first.children.last.to_s == name_part
+        end
+      end
+      current_node
+    end
+  end
+
+  class MethodNode < BaseNode
+    def method_call
+      name
+    end
+
+    def name
+      raw_node.children.first.to_s
+    end
+
+    def return_value
+      return_expression = raw_node.children.last
+      case return_expression.type
+      when :true
+        'true'
+      when :str
+        "'#{return_expression.children.last}'"
+      when :int
+        return_expression.children.last.to_s
+      when :lvar
+        binding.irb
+        return_expression.children.last.to_s
+      else
+        binding.irb
+        raise "unhandled type: #{return_expression.type}"
+      end
+    end
+  end
+
+  class ClassNode < BaseNode
+    def find_method(method_name)
+      MethodNode.new(find_method_node(raw_node, method_name), parent_node: self)
+    end
+
+    def find_method_node(raw_node, method_name)
+      raw_node.children.detect do |child_node|
+        next unless child_node
+        child_node.type == :def ||
+          child_node.type == :begin && find_method_node(child_node, method_name)
+      end
+    end
+  end
+
   class Composer
     TEMPLATE = File.read(Pathname.new(__dir__).join('../templates/spec.erb'))
 
@@ -9,52 +83,19 @@ module Buttress
     end
 
     def call(code, class_name, method_name)
-      ast = Parser::Ruby18.parse(code)
-      class_node = find_class_node(ast, class_name)
-      method_node = find_method_node(class_node, method_name)
+      root_node = RootNode.new(Parser::Ruby18.parse(code))
+      class_node = root_node.find_class(class_name)
+      method_node = class_node.find_method(method_name)
 
       # need to get a better return value
-      return_value = detect_return_value(method_node)
+      return_value = method_node.return_value
+      method_call = method_node.method_call
       instance_name = class_name.underscore.split('/').last
 
       ERB.new(TEMPLATE).result(binding)
     end
 
-    def find_class_node(node, class_name)
-      return unless node
-      return node if node.type == :module || node.type == :class &&
-        node.children.first.children.last.to_s == class_name
 
-      current_node = node
-      class_name.split('::').each do |name_part|
-        current_node = current_node.children.detect do |child|
-          next unless child
-          (child.type == :module || child.type == :class) &&
-          child.children.first.children.last.to_s == name_part
-        end
-      end
-      current_node
-    end
 
-    def find_method_node(node, method_name)
-      node.children.detect do |child|
-        next unless child
-        child.type == :def ||
-          child.type == :begin && find_method_node(child, method_name)
-      end
-    end
-
-    def detect_return_value(method_node)
-      # binding.irb
-      return_expression = method_node.children.last
-      case return_expression.type
-      when :true
-        'true'
-      when :str
-        "'#{return_expression.children.last}'"
-      else
-        raise "unhandled type: #{return_expression.type}"
-      end
-    end
   end
 end
--- spec/buttress/composer_spec.rb
+++ spec/buttress/composer_spec.rb
@@ -44,4 +44,48 @@ RSpec.describe Buttress::Composer, '#call' do
     expect(described_class.call(code, 'MyClass', 'call_me')).to eq(expected_tests)
   end
 
+  it 'returns test code for when method returns a number' do
+    code = <<~RUBY
+      class MyClass
+        def call_me
+          5
+        end
+      end
+    RUBY
+
+    expected_tests = <<~RUBY
+      describe MyClass, '#call_me' do
+        it 'returns 5' do
+          my_class = MyClass.new
+
+          my_class.call_me.should == 5
+        end
+      end
+    RUBY
+
+    expect(described_class.call(code, 'MyClass', 'call_me')).to eq(expected_tests)
+  end
+
+  xit 'returns test code for when method returns given value' do
+    code = <<~RUBY
+      class MyClass
+        def call_me(value)
+          value
+        end
+      end
+    RUBY
+
+    expected_tests = <<~RUBY
+      describe MyClass, '#call_me' do
+        it 'returns the given value' do
+          my_class = MyClass.new
+
+          my_class.call_me('blah').should == 'blah'
+        end
+      end
+    RUBY
+
+    expect(described_class.call(code, 'MyClass', 'call_me')).to eq(expected_tests)
+  end
+
 end
